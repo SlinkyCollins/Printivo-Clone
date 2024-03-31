0632512a6168dc5e64efbd2116c205e936f46f82
Ademola Afolabi <afolabiademola27@gmail.com>
2024-03-31 20:29:51 +0100

    Responsiveness finished

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -5,19 +5,19 @@ import qualityIcon from "../assets/topquality.svg";
 const Feature = () => {
     return (
         <>
-            <div style={{margin: "100px 50px 100px 60px"}}>
-                <h1 style={{color: "#384a62", fontFamily: "Times new roman, sans-serif", margin: "0px 0px 20px"}}>You can count on us for:</h1>
+            <div className="featureContainer" style={{margin: "100px 50px 100px 60px"}}>
+                <h1 className="featureContainerHeader" style={{color: "#384a62", fontFamily: "Times new roman, sans-serif", margin: "0px 0px 20px"}}>You can count on us for:</h1>
                 <div className="featureChildContainer" style={{display: "flex"}}>
-                    <div style={{display: "flex"}}>
+                    <div className="featureWrapper" style={{display: "flex"}}>
                         <div style={{margin: "0px 10px 0px 0px"}}>
                             <img src={fastIcon} />
                         </div>
-                        <div >
+                        <div>
                             <h1 style={{color: "#445971", fontSize: "15px", fontWeight: "600", margin: "0px 0px 8px"}}>Fast Turnaround Within Days</h1>
                             <p style={{color: "#7b9cc0", fontSize: "15px", width: "80%"}}>Your order gets to you within 3-7 working days via DHL or our very own Printivo Direct.</p>
                         </div>
                     </div>
-                    <div style={{display: "flex"}}>
+                    <div className="featureWrapper" style={{display: "flex"}}>
                         <div style={{margin: "0px 10px 0px 0px"}}>
                             <img src={affordIcon} />
                         </div>
@@ -26,7 +26,7 @@ const Feature = () => {
                             <p style={{color: "#7b9cc0", fontSize: "15px", width: "80%"}}>Only the finest materials, machines and people will be involved in fulfilling your order.</p>
                         </div>
                     </div>
-                    <div style={{display: "flex"}}>
+                    <div className="featureWrapper" style={{display: "flex"}}>
                         <div style={{margin: "0px 10px 0px 0px"}}>
                             <img src={qualityIcon} />
                         </div>
